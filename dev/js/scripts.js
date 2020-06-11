39219025fb62f5394b2163e6a1b57eb417e2bd25
serafla <63385649+serafla@users.noreply.github.com>
2020-06-11 14:29:07 -0400

    Initial commit

---
+++ dev/js/scripts.js
@@ -0,0 +1,27 @@
+
+
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+
+function mouseClick(){
+
+    //console.log("clicked!!!!!!!!!!!!!!!!!!");
+
+    //console.log("click");
+
+
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+$burger.on( "click", mouseClick);
+
+$navA.on( "click", mouseClick);
